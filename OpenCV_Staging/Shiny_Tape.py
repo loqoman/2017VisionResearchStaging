d9aaad2d7fd9fa333f922e357ae5075ef8d95b7f
Darwin Clark <darwin.s.clark@gmail.com>
2017-07-27 14:20:06 -0700

    Wrote the reflective tape program. Details will be added in readme:

--- OpenCV_Staging/Shiny_Tape.py
+++ OpenCV_Staging/Shiny_Tape.py
@@ -15,22 +15,55 @@ while(True):
 
     blur = cv2.medianBlur(frame,13)
     #result is dilated for marking the corners, not important
-    #edges = cv2.Canny(blur,60,0)
-    edges = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
+
+    '''
+    Filtering the white
+    '''
+    hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+
+    #In testing!!!
+    sensitivity = 3
+    lower_white = np.array([0,0,255-sensitivity])
+    upper_white = np.array([255,sensitivity,255])
+
+    mask = cv2.inRange(hsv, lower_white, upper_white)
+    # Frame is target, res is output
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+    #Grayscale
+    draw = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)    
+
+
+
+    edges = cv2.Canny(draw,60,0)
     
-    draw = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
-    # Finding contours
-    contours,h = cv2.findContours(draw,1,2)
-    print(contours)
+    #Perhaps gray, then edges?
+
+    # Finding contours from *edges*
+    contours,h = cv2.findContours(edges,1,2)
+    #print(contours)
 
     #In the form of (contours, eplison, True[?])
+    num_squares = 0
     for cnt in contours:
+	num_squares += 1 
+	epsilon = 0.1*cv2.arcLength(cnt,True)   
+	approx = cnt #cv2.approxPolyDP(cnt,epsilon,True) 	
+	# Approximating the rectangles basically is bad with the amount of light filtering going on.
 
-    
-        rect = cv2.minAreaRect(cnt)
-        box = cv2.cv.BoxPoints(rect)
-        box = np.int0(box)
-        cv2.drawContours(draw,[box],0,(0,0,255),2)
+    #cv2.drawContours(draw, contours, -1, (0,255,0), 3)
+        rect = cv2.minAreaRect(approx)
+
+	tilt = int(rect[2])
+        print("Rect[2] is currently at: ", tilt)
+	if (rect[2] > -100) & (rect[2] < -70): #Looking for squares that are only straight  
+            #Low point         High point
+            #print("Passed!")
+            box = cv2.cv.BoxPoints(rect)
+	    box = np.int0(box)
+	
+	    cv2.drawContours(draw,[box],0,(0,0,255),2)
+	    cv2.drawContours(frame,[box],0,(0,0,255),2)
+	    print("The center of square " ,num_squares , "Is at: " , box[1])
 
     # Threshold for an optimal value, it may vary depending on the image.
 
@@ -39,6 +72,8 @@ while(True):
     cv2.imshow('blur',blur)
     cv2.imshow('Draw',draw)
     cv2.imshow('Edges',edges)
+    cv2.imshow('frame', frame)
+    #cv2.imshow('grey',gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
